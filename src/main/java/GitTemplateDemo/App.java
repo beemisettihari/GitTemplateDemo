5bafa81b1f77f5490662da1a154b81955f5e2146
HariKrishna Beemisetti <hari308@gmail.com>
2022-04-23 12:54:57 +0530

    test123

--- src/main/java/GitTemplateDemo/App.java
+++ src/main/java/GitTemplateDemo/App.java
@@ -5,7 +5,7 @@ package GitTemplateDemo;
 
 public class App {
     public String getGreeting() {
-        return "Hello world!";
+        return "Hello world! s";
     }
 
     public static void main(String[] args) {
