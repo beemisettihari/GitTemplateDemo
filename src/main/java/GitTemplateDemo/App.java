77ea8db97e310f2ff9552d84449ebab70129e6c5
HariKrishna Beemisetti <hari308@gmail.com>
2025-04-07 22:03:33 +0530

    test

--- src/main/java/GitTemplateDemo/App.java
+++ src/main/java/GitTemplateDemo/App.java
@@ -10,6 +10,13 @@ public class App {
 
     String test;
 
+    public boolean isNameEmpty(String givenName, String familyName) {
+        if (givenName != null && familyName != null) {
+            return givenName.isEmpty() && familyName.isEmpty();
+        }
+        return familyName.isEmpty(); // Noncompliant; familyName may be null
+    }
+
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
     }
