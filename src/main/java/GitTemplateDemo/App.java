58d2f34433ee219d4042c3a9a1d9150b8786c66f
HariKrishna Beemisetti <hari308@gmail.com>
2025-04-07 21:51:45 +0530

    Test commit

--- src/main/java/GitTemplateDemo/App.java
+++ src/main/java/GitTemplateDemo/App.java
@@ -4,10 +4,34 @@
 package GitTemplateDemo;
 
 public class App {
+
+    String test;
+    /**
+     * Returns a greeting message.
+     * 
+     * @return A string containing the greeting message "Hello world."
+     */
     public String getGreeting() {
         return "Hello world.";
     }
 
+    /**
+     * Adds two integers and returns the result.
+     * 
+     * @param a The first integer.
+     * @param b The second integer.
+     * @return The sum of the two integers.
+     */
+    public int addNumbers(int a, int b) {
+        return a + b;
+    }
+
+    /**
+     * The main method serves as the entry point of the application.
+     * It prints the greeting message to the console.
+     * 
+     * @param args Command-line arguments (not used in this application).
+     */
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
     }
